344b5102b8f60b2629344d987e4ef21f41895cfd
LauraHexx <l.hesidenz97@gmail.com>
2023-01-10 15:56:20 +0100

    camera + background

--- models/world.class.js
+++ models/world.class.js
@@ -8,10 +8,31 @@ class World {
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
   backgroundObjects = [
+    //Todo: For - Schleife
+    new BackgroundObject("img/5_background/layers/air.png", -719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
+
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+
+    new BackgroundObject("img/5_background/layers/air.png", 1438),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 1438),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 1438),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 1438),
+
+    new BackgroundObject("img/5_background/layers/air.png", 2157),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 2157),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 2157),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 2157),
   ];
 
   constructor(canvas, keyboard) {
