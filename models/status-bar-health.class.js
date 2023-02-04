09860222c3b81af6cf88e7fe698e2c305b6fa739
LauraHexx <l.hesidenz97@gmail.com>
2023-02-04 23:23:13 +0100

    small changes

--- models/status-bar-health.class.js
+++ models/status-bar-health.class.js
@@ -5,12 +5,12 @@ class StatusBarHealth extends DrawableObject {
   height = 60;
   percentage = 100;
   imagesBarHealth = [
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png",
   ];
 
   constructor() {
