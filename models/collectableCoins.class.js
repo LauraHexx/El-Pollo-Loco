513eac8329a0f256716172846d8df7f3180bc4d6
LauraHexx <l.hesidenz97@gmail.com>
2023-02-05 14:04:12 +0100

    coin

--- models/collectableCoins.class.js
+++ models/collectableCoins.class.js
@@ -1,9 +1,21 @@
-class CollectableCoin extends DrawableObject {
+class CollectableCoin extends MoveableObject {
+  x = 100;
+  y = 100;
+  width = 80;
+  height = 80;
   imagesCoin = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
+
   constructor(x, y) {
     super().loadImage("img/8_coin/coin_1.png");
+    this.loadImages(this.imagesCoin);
     this.x = x;
     this.y = y;
-    this.loadImages(this.imagesCoin);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.imagesCoin);
+    }, 100);
   }
 }
