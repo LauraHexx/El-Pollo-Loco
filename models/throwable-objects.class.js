4713d4afdd0b44a0b161cc0d91223d1441a27aaa
LauraHexx <l.hesidenz97@gmail.com>
2023-03-15 22:58:54 +0100

    throwable object small changes

--- models/throwable-objects.class.js
+++ models/throwable-objects.class.js
@@ -1,9 +1,7 @@
 class ThrowableObject extends MoveableObject {
   width = 60;
   height = 80;
-  speedY = 15;
-  speedX = 10;
-  acceleration = 2.5;
+
   imagesThrowing = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -18,7 +16,9 @@ class ThrowableObject extends MoveableObject {
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
-
+  speedY = 15;
+  speedX = 10;
+  acceleration = 2.5;
   isSmashed = false;
 
   constructor(x, y) {
