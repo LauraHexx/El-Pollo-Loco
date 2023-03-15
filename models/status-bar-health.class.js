6a1eef61a04ec5b04b7c76062e3d16b7dcc62cf5
LauraHexx <l.hesidenz97@gmail.com>
2023-03-15 22:53:00 +0100

    refactoring StatusBarHealth

--- models/status-bar-health.class.js
+++ models/status-bar-health.class.js
@@ -1,6 +1,6 @@
 class StatusBarHealth extends DrawableObject {
   x = 40;
-  y = 00;
+  y = 0;
   width = 200;
   height = 60;
   percentage = 100;
@@ -26,18 +26,6 @@ class StatusBarHealth extends DrawableObject {
   }
 
   getIndexImage() {
-    if (this.percentage == 100) {
-      return 5;
-    } else if (this.percentage == 80) {
-      return 4;
-    } else if (this.percentage == 60) {
-      return 3;
-    } else if (this.percentage == 40) {
-      return 2;
-    } else if (this.percentage == 20) {
-      return 1;
-    } else {
-      return 0;
-    }
+    return this.percentage / 20;
   }
 }
