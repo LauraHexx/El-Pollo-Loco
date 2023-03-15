84d8df9bbf2f5b4d3019be742c97ea01f6ad8aa9
LauraHexx <l.hesidenz97@gmail.com>
2023-03-15 22:56:27 +0100

    refactoring statusbar bottle + coin

--- models/status-bar-bottle.class.js
+++ models/status-bar-bottle.class.js
@@ -26,11 +26,6 @@ class StatusBarBottle extends DrawableObject {
   }
 
   getIndexImage() {
-    if (this.percentage == 5) return 5;
-    else if (this.percentage == 4) return 4;
-    else if (this.percentage == 3) return 3;
-    else if (this.percentage == 2) return 2;
-    else if (this.percentage == 1) return 1;
-    else return 0;
+    return this.percentage;
   }
 }
