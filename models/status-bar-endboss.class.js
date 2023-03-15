75877099c9143e6462798b20d1f6db7d0741b7b7
LauraHexx <l.hesidenz97@gmail.com>
2023-03-15 22:57:06 +0100

    refactoring statusbar health

--- models/status-bar-endboss.class.js
+++ models/status-bar-endboss.class.js
@@ -25,18 +25,6 @@ class StatusBarEndboss extends DrawableObject {
   }
 
   getIndexImage() {
-    if (this.percentage == 100) {
-      return 5;
-    } else if (this.percentage == 80) {
-      return 4;
-    } else if (this.percentage == 60) {
-      return 3;
-    } else if (this.percentage == 40) {
-      return 2;
-    } else if (this.percentage == 20) {
-      return 1;
-    } else {
-      return 0;
-    }
+    return this.percentage / 20;
   }
 }
